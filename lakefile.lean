2246e7058584de2a082d8e9fc470305cd805b078
staroperator <staroperator92@gmail.com>
2024-02-20 18:57:50 -0500

    init

---
+++ lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+open Lake DSL
+
+package «misc» where
+  -- add any package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
